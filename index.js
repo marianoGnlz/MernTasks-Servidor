a28a44473d783b72b765b00a676c60855aba433e
Mariano Gonzalez <marianognlz23@gmail.com>
2021-03-29 12:42:48 -0300

    cosas

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(cors());
 
 app.use(express.json({ extended: true }));
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-app.listen(PORT, () => {
-    console.log(`Servidor disponible en el puerto: ${PORT}`)
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Servidor disponible en el puerto: ${port}`)
 })
